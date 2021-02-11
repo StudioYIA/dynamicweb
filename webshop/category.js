5926e8fab4c918be45802b874e90a35d6ccbdd45
Yolene Iracema Acke <iracema.acke@gmail.com>
2021-02-11 22:05:58 +0100

    productList alfabetical

--- webshop/category.js
+++ webshop/category.js
@@ -1,3 +1,33 @@
+//create navigation
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const letterArray = letters.split("");
+console.log(letterArray);
+letterArray.forEach(handleLetter);
+
+function handleLetter(letter) {
+  createNavLink(letter);
+
+  //create section
+  createBrandSection(letter);
+}
+
+function createBrandSection(letter) {
+  const template = document.querySelector("#sectionTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h2").textContent = letter;
+  copy.querySelector("section").id = `letter_${letter}`;
+  document.querySelector(".brandList").appendChild(copy);
+}
+
+function createNavLink(letter) {
+  //grab the template, copy, add content, append
+  const template = document.querySelector("#linkTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("a").textContent = letter;
+  copy.querySelector("a").href = `#letter_${letter}`;
+  document.querySelector(".letterLinks ol").appendChild(copy);
+}
+
 //fetch data
 fetch("https://kea-alt-del.dk/t7/api/brands")
   .then((res) => res.json())
@@ -10,13 +40,15 @@ function gotData(data) {
 }
 function showBrand(brand) {
   console.log(brand.brandname);
-  const template = document.querySelector("template").content;
+  const template = document.querySelector("#linkTemplate").content;
   const copy = template.cloneNode(true);
   copy.querySelector("a").textContent = brand.brandname;
   copy.querySelector(
     "a"
   ).href = `productlist.html?brandname=${brand.brandname}`;
-  const topParent = document.querySelector("#letter_a");
+  const firstLetter = brand.brandname[0].toLowerCase();
+  console.log(firstLetter);
+  const topParent = document.querySelector(`#letter_${firstLetter}`);
   const elemParent = topParent.querySelector("ol");
   elemParent.appendChild(copy);
 }
